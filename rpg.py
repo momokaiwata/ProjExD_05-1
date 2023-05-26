b7c89371286e083d6464114dbc9ead77fca1085d
ayaka_s <c0b2206820@edu.teu.ac.jp>
2023-05-26 13:11:08 +0900

    コメントアウト追加

--- rpg.py
+++ rpg.py
@@ -3,12 +3,13 @@ import random
 import sys
 import pygame as pg
 
-WIDTH = 1600
-HIGHT = 900
-txt_origin = ["攻撃","防御","魔法","回復","調教","逃走"]
-HP = 50
-MP = 10
-ENE_HP = 10
+# global変数
+WIDTH = 1600    # ウィンドウの横幅
+HIGHT = 900     # ウィンドウの縦幅
+txt_origin = ["攻撃","防御","魔法","回復","調教","逃走"]    # 勇者の行動選択肢のリスト
+HP = 50         # 勇者のHP
+MP = 10         # 勇者のMP
+ENE_HP = 10     # 敵スライムのHP
 
 class Button:
     """
@@ -22,11 +23,11 @@ class Button:
         width: ボタンの横幅
         height: ボタンの縦幅
         color: ボタンの色
-        hover_color: ？
+        hover_color: マウスカーソルがボタンの上にある時のボタンの色
         text: 行動選択肢の文字
         text_color: 文字の色
-        action: 行動
-        num: ？
+        action: action関数
+        num: index(0:攻撃, 1:防御, 2:魔法, 3:回復, 4:調教, 5:逃走)
         """
         self.rect = pg.Rect(x, y, width, height)    # rectを四角形を描画するsurfaceで初期化
         self.color = color
@@ -39,7 +40,7 @@ class Button:
     def draw(self,scr):
         """
         ボタンを描画するメソッド
-        scr: screen
+        scr: surface
         """
         pg.draw.rect(scr, self.color, self.rect)    # ボタンとなる四角形を描画
         font = pg.font.SysFont("hg正楷書体pro", 50)  # フォント指定
@@ -59,7 +60,8 @@ class Button:
         
 def action(i):
     """
-    勇者の行動に関する関数
+    勇者の行動をプリントする関数
+    i: index (0:攻撃, 1:防御, 2:魔法, 3:回復, 4:調教, 5:逃走)
     """
     p = ["攻撃","防御","魔法","回復","調教","逃走"]
     print(p[i])
@@ -68,54 +70,67 @@ def main():
     """
     main関数
     """
-    global WIDTH,HIGHT,txt_t,txt_origin
+    global WIDTH, HIGHT, txt_t, txt_origin     # global変数
 
     bg_image = "./ex05/fig/back.png"
-    pg.display.set_caption("RPG初期段階")
-    screen = pg.display.set_mode((WIDTH, HIGHT))
-    clock  = pg.time.Clock()
+    pg.display.set_caption("RPG of くそげー")   # ウィンドウの名前
+    screen = pg.display.set_mode((WIDTH, HIGHT))    # 1600x900のdisplay surface
+    clock  = pg.time.Clock()                        # 時間
+    # surface
+    # 背景
     bg_img = pg.image.load(bg_image)
     bg_img = pg.transform.scale(bg_img,(WIDTH,HIGHT))
+    # 敵スライム
     ene_img = pg.image.load("./ex05/fig/ene.png")
     ene_rct = ene_img.get_rect()
+    # テキストボックス
     win = pg.image.load("./ex05/fig/win.png")
     win = pg.transform.scale(win,(WIDTH/4,HIGHT/2))
     win2 = pg.transform.scale(win,(WIDTH-100,HIGHT/4))
+    # フォント
     font1 = pg.font.SysFont("hg正楷書体pro", 100)
     font2 = pg.font.SysFont("hg正楷書体pro", 50)
+    # テキスト
     text = "野生のスライムが現れた"
-    txt = []
-    text_surface1 = font2.render(f"HP:{HP} MP:{MP}", True, (255,255,255))
-    text_surface2 = font2.render(f"HP:{ENE_HP}", True, (255,255,255))
+    txt = []    # 選択ボタンを描画するsurfaceのリスト
+    text_surface1 = font2.render(f"HP:{HP} MP:{MP}", True, (255,255,255))   # 勇者のHP,MPのテキストsurface
+    text_surface2 = font2.render(f"HP:{ENE_HP}", True, (255,255,255))       # 敵スライムのHPのテキストsurface
+    # 勇者の行動選択ボタンを描画するsurfaceを作成しリストtxtに追加
     for i,tx in enumerate(txt_origin):
+        # インスタンス化
         if i%2==0:
             button = Button(125, 500+(i//2)*100, 100, 50, (50,50,50), (0,0,0), tx, (255,255,255), action, i)
         else:
             button = Button(275, 500+(i//2)*100, 100, 50, (50,50,50), (0,0,0), tx, (255,255,255), action, i)
         txt.append(button)
-        
+
+    # 繰り返し文    
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
-            for button in txt:
+            if event.type == pg.QUIT: return    # ×ボタンが押されたらプログラム終了
+
+            for button in txt:                  # ？
                 button.handle_event(event)
 
-        screen.blit(bg_img,[0,0])
-        screen.blit(ene_img,[WIDTH/2-ene_rct.width/2+100,HIGHT/2])
-        screen.blit(win,[50,400])
-        screen.blit(win2,[50,50])
-        x=200
+        screen.blit(bg_img,[0, 0])      # 背景描画
+        screen.blit(ene_img,[WIDTH/2-ene_rct.width/2+100, HIGHT/2]) # 敵スライム描画
+        screen.blit(win,[50, 400])      # テキストボックス描画
+        screen.blit(win2,[50, 50])      # 行動選択のテキストボックス描画
+
+        x = 200
         for chr in text:
             rendered_text = font1.render(chr, True, (255, 255, 255))
             text_width = rendered_text.get_width()
-            screen.blit(rendered_text,[x,100])
+            screen.blit(rendered_text,[x, 100])
             x += text_width
         for i in txt:
-            i.draw(screen)
-        screen.blit(text_surface1,[100,350])
-        screen.blit(text_surface2,[WIDTH/2-ene_rct.width/2+225,HIGHT/2-50])
-        pg.display.update()
-        clock.tick(100)
+            i.draw(screen)  # ボタン描画
+
+        screen.blit(text_surface1,[100, 350])   # 勇者のHP,MP表示
+        screen.blit(text_surface2,[WIDTH/2-ene_rct.width/2+225, HIGHT/2-50])    # 敵スライムのHP表示
+        
+        pg.display.update()     # ディスプレイのアップデート
+        clock.tick(100)         # 時間
 
 if __name__ == "__main__":
     pg.init()
